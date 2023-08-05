b045b85c0c183bd942e3767efe86e9c3efe0a14e
Shantanu Sutar <shantanusutar17@gmail.com>
2023-08-05 20:43:26 +0530

    styling header and adding input fields from Material UI and creating dark theme

--- word-hunt/src/components/Header/Header.jsx
+++ word-hunt/src/components/Header/Header.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import "./Header.css";
+import { MenuItem, TextField, ThemeProvider, createTheme } from "@mui/material";
 
 const Header = () => {
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      mode: "dark",
+    },
+  });
+
   return (
     <div className="header">
       <span className="title">Word Hunt</span>
+      <div className="inputs">
+        <ThemeProvider theme={darkTheme}>
+          <TextField id="standard-basic" label="Standard" variant="standard" />
+
+          <TextField
+            id="standard-select-currency"
+            select
+            label="Select"
+            helperText="Please select your currency"
+            variant="standard"
+          >
+            <MenuItem>english</MenuItem>
+          </TextField>
+        </ThemeProvider>
+      </div>
     </div>
   );
 };
