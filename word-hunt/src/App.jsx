87bf1f5606e71eea5fd3f8192b6f7c5dc26d93af
Shantanu Sutar <shantanusutar17@gmail.com>
2023-08-05 19:52:01 +0530

    basic styling and creating Header

--- word-hunt/src/App.jsx
+++ word-hunt/src/App.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { Container } from "@mui/material";
 import axios from "axios";
+import Header from "./components/Header/Header";
 
 function App() {
+  const [word, setWord] = useState();
   const [meanings, setMeanings] = useState([]);
 
   const dictionaryApi = async () => {
@@ -25,8 +28,16 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>Shantanu</h1>
+    <div
+      className="App"
+      style={{ height: "100vh", backgroundColor: "#282c34", color: "white" }}
+    >
+      <Container
+        maxWidth="md"
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        <Header />
+      </Container>
     </div>
   );
 }
