fbea0f66ae973f2b23b156ef67e749732225bc12
Shantanu Sutar <shantanusutar17@gmail.com>
2023-08-05 19:42:57 +0530

    fetching data from dictionary API using Axios

--- word-hunt/src/App.jsx
+++ word-hunt/src/App.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [meanings, setMeanings] = useState([]);
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
+      );
+
+      // console.log(data);
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+
   return (
     <div>
       <h1>Shantanu</h1>
