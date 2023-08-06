b629e231b92a3091d02998dca86858e7ccecd3a8
Shantanu Sutar <shantanusutar17@gmail.com>
2023-08-06 11:59:15 +0530

    creating a custom color switch with Material UI

--- word-hunt/src/App.jsx
+++ word-hunt/src/App.jsx
@@ -1,14 +1,27 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { Container } from "@mui/material";
+import { Container, Switch } from "@mui/material";
 import axios from "axios";
 import Header from "./components/Header/Header";
 import Definitions from "./components/Definitions/Definitions";
+import styled from "@emotion/styled";
+import { grey } from "@mui/material/colors";
 
 function App() {
   const [word, setWord] = useState("");
   const [meanings, setMeanings] = useState([]);
   const [category, setCategory] = useState("en");
+
+  const label = { inputProps: { "aria-label": "Color switch demo" } };
+
+  const CustomSwitch = styled(Switch)(({ theme }) => ({
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      color: grey[600],
+    },
+    "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+      backgroundColor: grey[600],
+    },
+  }));
 
   const dictionaryApi = async () => {
     try {
@@ -36,6 +49,11 @@ function App() {
         maxWidth="md"
         style={{ display: "flex", flexDirection: "column", height: "100vh" }}
       >
+        <div
+          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
+        >
+          <CustomSwitch {...label} defaultChecked />
+        </div>
         <Header
           word={word}
           setWord={setWord}
